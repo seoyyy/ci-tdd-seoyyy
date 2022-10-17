b471de77d1092a8674192642c194a1f4f2caa824
“seoyyy” <seoyyy@gmail.com>
2022-10-17 16:55:43 +0900

    todoItem refactoring ok

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 const TodoItem = ({todo , onToggle, onRemove}) =>{
     const {id, text, done} = todo;
+    const toggle = useCallback(() => onToggle(id), [onToggle,id]);
+    const remove = useCallback(() => onRemove(id), [onRemove,id]);
+
     return (
     <li>
-        <span style={{ // 삼항연산자를 이용해 done이라는 변수가 들어오며 줄을 그어서 출력하고 아니면 아무것도하지마라
+        <span 
+        style={{ // 삼항연산자를 이용해 done이라는 변수가 들어오며 줄을 그어서 출력하고 아니면 아무것도하지마라
             textDecoration: done ? 'line-through' : 'none'
         }}
-        onClick={() => onToggle(id)}>{text}</span>
-        <button onClick={() => onRemove(id)}>삭제</button>
+        onClick={toggle}>{text}</span>
+        <button onClick={remove}>삭제</button>
     </li>);
 };
 export default TodoItem;
