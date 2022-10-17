975caa5de3302a9631affe5ea1b50e822756a8f5
“seoyyy” <seoyyy@gmail.com>
2022-10-17 16:51:32 +0900

    TodoItem clickevent test ok

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,13 +1,14 @@
 import React from "react";
 
-const TodoItem = ({todo}) =>{
+const TodoItem = ({todo , onToggle, onRemove}) =>{
     const {id, text, done} = todo;
     return (
     <li>
         <span style={{ // 삼항연산자를 이용해 done이라는 변수가 들어오며 줄을 그어서 출력하고 아니면 아무것도하지마라
             textDecoration: done ? 'line-through' : 'none'
-        }}>{text}</span>
-        <button>삭제</button>
+        }}
+        onClick={() => onToggle(id)}>{text}</span>
+        <button onClick={() => onRemove(id)}>삭제</button>
     </li>);
 };
 export default TodoItem;
--- src/TodoItem.test.js
+++ src/TodoItem.test.js
@@ -37,7 +37,22 @@ describe('<TodoItem />', () => {
         const{span} = setup({todo: {...sampleTodo, done: false } });
         expect(span).not.toHaveStyle('text-decoration: line-through;');
     });
+    
 
+    it('calls onToggle', () => {
+        //onToggle이라는 함수가 호출이 됐나 안됐나 확인
+        const onToggle = jest.fn();
+        const {span} =setup({onToggle});
+        //span태그가 클릭됬는지 확인. 클릭이 되었을때 onToggle이라는 함수가 호출되도록 체크하기전에
+        fireEvent.click(span);
+        expect(onToggle).toBeCalledWith(sampleTodo.id); //onToggle이 호출이 됫을때 id가 1이라는 애로 호출이 됐니?
+    });
 
-
+    //삭제 버튼 눌럿을때 삭제 되도록
+    it('calls onRemove', () => {
+        const onRemove = jest.fn();
+        const {button} =setup({onRemove});  //버튼이 넘어와서
+        fireEvent.click(button); //버튼을 클릭하면
+        expect(onRemove).toBeCalledWith(sampleTodo.id); //onRemove 함수가 호출이 되느냐
+    });
 });
