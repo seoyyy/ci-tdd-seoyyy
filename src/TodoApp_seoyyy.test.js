2311792e4bad9f46e4cecc084b14ccb371e77528
“seoyyy” <seoyyy@gmail.com>
2022-10-18 11:31:55 +0900

    TodoApp TodoForm TodoList render test ok

--- src/TodoApp_seoyyy.test.js
+++ src/TodoApp_seoyyy.test.js
@@ -8,4 +8,9 @@ describe('<TodoApp_seoyyy />', () => {
             getByText('등록'); //TodoFrom 화면에 있는지 확인
             getByTestId('TodoList'); //TodoList 화면에 있는지 확인
     });
+    it('renders two defaults todos', () => {
+        const {getByText} = render(<TodoApp_seoyyy />);
+        getByText('TDD 배우기');
+        getByText('react-testing-library');
+    });
 });
